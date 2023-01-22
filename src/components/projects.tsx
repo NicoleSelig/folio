3b06d7d89fffebb6bde1cc64b3b6d5a289637ca7
Nicole Selig <nicoleleeselig@gmail.com>
2023-01-22 13:32:39 -0500

    add portfolio site to list of projects

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,6 +3,7 @@ import SectionLabel from "./sectionLabel";
 import HYSImage from '../../public/assets/projects/help-your-selves.png'
 import PortalImage from '../../public/assets/projects/portal-scene.png'
 import RedPandaImage from '../../public/assets/projects/red-panda.png'
+import FolioImage from '../../public/assets/projects/portfolio-site.png'
 import Project from "./project";
 
 export default function Projects() {
@@ -14,6 +15,7 @@ export default function Projects() {
             <Project name="Red Panda Scouting Companion" tech="-Next.js -Javascript -Azure CosmosDB -Azure Web Apps -Terraform -GitHub Actions" image={RedPandaImage} externalLink="https://testlolscoutwa01@azurewebsites.net" githubLink="https://github.com/LOLRedPanda/app-core" />
             <Project name="The Pink Portal" tech="-THREE.js -React THREE Fiber" image={PortalImage} externalLink="https://threejs-journey-two.vercel.app/" githubLink="https://github.com/NicoleSelig/threejs-journey/tree/master/26%20-%20portal%20scene%20react%20three%20fiber" />
             <Project name="Help Your Selves Puzzle Game" tech="-Unity -C#" image={HYSImage} externalLink="https://wwwsepcom.itch.io/help-your-selves" githubLink="https://github.com/NicoleSelig/help-your-selves" />
+            <Project name="My Personal Website" tech="Next.js" image={FolioImage} externalLink="" githubLink="https://github.com/NicoleSelig/folio" />
         </div>
       </div>
     </div>
